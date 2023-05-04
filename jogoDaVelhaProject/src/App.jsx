41efc6380ad55d6f610c4e3cd408f514191e7a07
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-05-04 14:27:45 -0300

    Fix: Corrigindo bug

--- jogoDaVelhaProject/src/App.jsx
+++ jogoDaVelhaProject/src/App.jsx
@@ -1,44 +1,185 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Box from "./components/molecules/Box";
 
 function App() {
   const [token, setToken] = useState(null);
-  const [board, setBoard] = useState(new Array(9).fill("0"));
+  const [secondToken, setSecondToken] = useState(null);
+  const [board, setBoard] = useState(new Array(9).fill(""));
+  const [actualPlayer, setActualPlayer] = useState(0);
+  const [playerWinner, setPlayerWinner] = useState("");
+
+  const checkLines = (checkToken, player) => {
+    if (
+      board[0] === checkToken &&
+      board[1] === checkToken &&
+      board[2] === checkToken
+    ) {
+      setPlayerWinner(player);
+    }
+    if (
+      board[3] === checkToken &&
+      board[4] === checkToken &&
+      board[5] === checkToken
+    ) {
+      setPlayerWinner(player);
+    }
+    if (
+      board[6] === checkToken &&
+      board[7] === checkToken &&
+      board[8] === checkToken
+    ) {
+      setPlayerWinner(player);
+    }
+  };
+
+  const checkVert = (checkToken, player) => {
+    if (
+      board[0] === checkToken &&
+      board[3] === checkToken &&
+      board[6] === checkToken
+    ) {
+      setPlayerWinner(player);
+    }
+    if (
+      board[1] === checkToken &&
+      board[4] === checkToken &&
+      board[7] === checkToken
+    ) {
+      setPlayerWinner(player);
+    }
+    if (
+      board[2] === checkToken &&
+      board[5] === checkToken &&
+      board[8] === checkToken
+    ) {
+      setPlayerWinner(player);
+    }
+  };
+
+  const checkDiag = (checkToken, player) => {
+    if (
+      board[0] === checkToken &&
+      board[4] === checkToken &&
+      board[8] === checkToken
+    ) {
+      setPlayerWinner(player);
+    }
+    if (
+      board[2] === checkToken &&
+      board[4] === checkToken &&
+      board[6] === checkToken
+    ) {
+      setPlayerWinner(player);
+    }
+  };
 
   useEffect(() => {
-    console.log(token);
-  }, [token]);
+    console.log(board);
+    checkLines(token, "Primeiro Jogador");
+    checkLines(secondToken, "Segundo Jogador");
+    checkVert(token, "Primeiro Jogador");
+    checkVert(secondToken, "Segundo Jogador");
+    checkDiag(token, "Primeiro Jogador");
+    checkDiag(secondToken, "Segundo Jogador");
+  }, [board]);
 
+  const resetBoard = () => {
+    /*
+      const [token, setToken] = useState(null);
+      const [secondToken, setSecondToken] = useState(null);
+      const [board, setBoard] = useState(new Array(9).fill(""));
+      const [actualPlayer, setActualPlayer] = useState(0);
+      const [playerWinner, setPlayerWinner] = useState("");
+    */
+
+    setToken(null);
+    setSecondToken(null);
+    setBoard(new Array(9).fill(""));
+    setActualPlayer(0);
+    setPlayerWinner("");
+  };
+
+  /*
+
+  Lógica do Jogo da Velha
+  
+  LINHAS 0 1 2 === Ganhou
+  LINHAS 3 4 5 === Ganhou
+  LINHAS 6 7 8 === Ganhou
+  
+  VERTICAL 0 3 6 === Ganhou
+  VERTICAL 1 4 7 === Ganhou
+  VERTICAL 2 5 8 === Ganhou
+
+  DIAGONAL 0 4 8 === Ganhou
+  DIAGONAL 2 4 6 === Ganhou
+
+  */
   return (
     <div className="App">
-      <h1>Jogo da Velha</h1>
-      <h3>Player 1: Escolha o seu token</h3>
-      {token === null && (
-        <div className="tokens">
-          <div className="token" onClick={() => setToken("O")}>
-            <img src="./src/assets/img/circle.png" />
+      {playerWinner && (
+        <>
+          <h1>{playerWinner} Ganhou!</h1>
+          <div className="buttonBox">
+            <button
+              className="resetButton"
+              onClick={() => {
+                resetBoard();
+              }}
+            >
+              Jogar Novamente
+            </button>
           </div>
-          <div className="token" onClick={() => setToken("X")}>
-            <img src="./src/assets/img/cross.png" />
-          </div>
-        </div>
+        </>
       )}
 
-      <div className="grid">
-        <div className="card">
-          <img
-            src={board[0] === "circle" ? "./src/assets/img/bigCircle.svg" : ""}
-          />
+      <>
+        <div className="boxLogo">
+          <img className="logo" src="./src/assets/img/favicon.png" />
         </div>
-        <div className="card">2</div>
-        <div className="card">3</div>
-        <div className="card">4</div>
-        <div className="card">5</div>
-        <div className="card">6</div>
-        <div className="card">7</div>
-        <div className="card">8</div>
-        <div className="card">9</div>
-      </div>
+        <h1>Jogo da Velha</h1>
+        {token === null && (
+          <>
+            <h3>Player 1: Escolha o seu token</h3>
+
+            <div className="tokens">
+              <div
+                className="token"
+                onClick={() => {
+                  setToken("O"), setSecondToken("X");
+                }}
+              >
+                <img src="./src/assets/img/circle.png" />
+              </div>
+              <div
+                className="token"
+                onClick={() => {
+                  setToken("X"), setSecondToken("O");
+                }}
+              >
+                <img src="./src/assets/img/cross.png" />
+              </div>
+            </div>
+          </>
+        )}
+
+        <div className="grid">
+          {board.map((index) => {
+            return (
+              <Box
+                board={board}
+                setBoard={setBoard}
+                token={token}
+                index={index}
+                setActualPlayer={setActualPlayer}
+                secondToken={secondToken}
+                actualPlayer={actualPlayer}
+              ></Box>
+            );
+          })}
+        </div>
+      </>
     </div>
   );
 }
---
+++ jogoDaVelhaProject/src/components/molecules/Box.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function Box({
+  board,
+  setBoard,
+  token,
+  index,
+  setActualPlayer,
+  secondToken,
+  actualPlayer,
+}) {
+  return (
+    <div
+      className="card"
+      onClick={() => {
+        setActualPlayer((prev) => {
+          const newPlayer = prev === 0 ? 1 : 0;
+          return newPlayer;
+        });
+
+        setBoard((prev) => {
+          prev[index] = actualPlayer === 0 ? token : secondToken;
+          return [...prev];
+        });
+      }}
+    >
+      <img
+        className="icon"
+        src={
+          board[index] === "O"
+            ? "./src/assets/img/bigCircle.svg"
+            : board[index] === "X"
+            ? "./src/assets/img/bigCross.svg"
+            : ""
+        }
+      />
+    </div>
+  );
+}
